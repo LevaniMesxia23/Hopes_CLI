ad12e177181d777a98a9011f0f378ff1dff47c72
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-03-02 20:44:30 +0400

    add findHope to find specific hope

--- src/hopes.js
+++ src/hopes.js
@@ -1,16 +1,23 @@
 import { getDB, saveDB, insertDB } from "./db.js";
 
-export const newHope = async (hope,tags) =>{
+export const newHope = async (hope, tags) => {
   const data = {
     tags,
     content: hope,
-    id: Date.now()
-  }
-  await insertDB(data)
-  return data
-}
+    id: Date.now(),
+  };
+  await insertDB(data);
+  return data;
+};
 
 export const getAllHopes = async () => {
-  const db = await getDB()
-  return db.hopes
-}
+  const db = await getDB();
+  return db.hopes;
+};
+
+export const findHope = async (filter) => {
+  const hopes = await getAllHopes();
+  return hopes.filter((hope) =>
+    hope.content.toLowerCase().includes(filter.toLowerCase())
+  );
+};
